cf56e4f29351b53d636beca5b15a64dedcb7c7fa
Connor Abdelnoor <connora@copiousinc.com>
2016-05-28 22:04:50 -0700

    add base libraries for backbone

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+    <head>
+        <meta charset="utf-8">
+        <title>Connor Abdelnoor</title>
+        <script src="./lib/backbone-1.3.3-min.js"/>
+        <script src="./lib/underscore-1.8.3.min.js"/>
+        <script src="./lib/jquery-1.12.4.min.js"/>
+    </head>
+    <body>
+        
+    </body>
+</html>
+
