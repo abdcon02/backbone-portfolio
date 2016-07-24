a45b034db07458cf8de6f560e3029f9768e706da
Connor Abdelnoor <connora@copiousinc.com>
2016-07-24 12:59:31 -0700

    change gulp workflow

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,22 @@
 'use strict'
 
-const gulp       = require('gulp'),
-    gutil      = require('gulp-util'),
-    watch      = require('gulp-watch'),
-    sass       = require('gulp-sass'),
-    rename     = require('gulp-rename'),
-    uglify     = require('gulp-uglify'),
-    imagemin   = require('gulp-imagemin'),
-    sourcemaps = require('gulp-sourcemaps'),
-    concat     = require('gulp-concat'),
-    livereload = require('gulp-livereload')
+const   gulp         = require('gulp'),
+        autoprefixer = require('gulp-autoprefixer'),
+        gutil        = require('gulp-util'),
+        watch        = require('gulp-watch'),
+        sass         = require('gulp-sass'),
+        rename       = require('gulp-rename'),
+        uglify       = require('gulp-uglify'),
+        imagemin     = require('gulp-imagemin'),
+        sourcemaps   = require('gulp-sourcemaps'),
+        concat       = require('gulp-concat'),
+        livereload   = require('gulp-livereload')
 
 const projectTheme = './'
+
+const config = Object.freeze({
+    production: !!gutil.env.production
+})
 
 const themePaths = Object.freeze({
     css: projectTheme + 'css',
@@ -20,6 +25,7 @@ const themePaths = Object.freeze({
 })
 
 const sourcePaths = Object.freeze({
+    browserSass: projectTheme + 'source/scss/styles.scss',
     sass: projectTheme + 'source/scss/**/*.scss',
     js : projectTheme + 'source/js/**/*.js',
     images: projectTheme + 'source/images/*',
@@ -27,15 +33,31 @@ const sourcePaths = Object.freeze({
     app: projectTheme + 'app/**/*'
 })
 
-gulp.task('default', ['compileSass', 'compressImages', 'compressJS'])
 
-gulp.task('compileSass', function () {
-    return gulp.src(sourcePaths.sass)
+gulp.task('default', ['compileSass', 'compressImages', 'compressJs'])
+
+const sassTask = function(srcPaths) {
+    return gulp.src(srcPaths)
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({
+            includePaths:
+                [
+                    require('bourbon').includePaths
+                ]
+        })
+            .on('error', sass.logError))
+        .pipe(autoprefixer())
         .pipe(sourcemaps.write('./source-maps'))
         .pipe(gulp.dest(themePaths.css))
-        .pipe(livereload())
+        .pipe(config.production ? gutil.noop() : livereload())
+}
+
+gulp.task('compileBrowserSass', function() {
+    return sassTask(sourcePaths.browserSass)
+})
+
+gulp.task('compileSass', function() {
+    return sassTask(sourcePaths.sass)
 })
 
 gulp.task('compressImages', function() {
@@ -45,18 +67,18 @@ gulp.task('compressImages', function() {
             svgoPlugins: [{removeViewBox: false}]
         }))
         .pipe(gulp.dest(themePaths.images))
-        .pipe(livereload())
+        .pipe(config.production ? livereload() : gutil.noop())
 })
 
-gulp.task('compressJS', function() {
+gulp.task('compressJs', function() {
     return gulp.src(sourcePaths.js)
-        .pipe(uglify())
+        .pipe(config.production ? uglify({compress: config.production, mangle: config.production}) : gutil.noop())
         .pipe(gulp.dest(themePaths.js))
-        .pipe(livereload())
+        .pipe(config.production ? livereload() : gutil.noop())
 })
 
 gulp.task('watch', function() {
-    gutil.log("starting to watch " + sourcePaths.path)
+    gutil.log('starting to watch ' + sourcePaths.path)
     livereload.listen()
-    gulp.watch([sourcePaths.path, sourcePaths.app], ['default'])
+    gulp.watch(sourcePaths.path, ['compileBrowserSass', 'compressJs'])
 })
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
-<?php
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="utf-8">
+    <title>Connor Abdelnoor</title>
+    <script src="lib/jquery-1.12.4.min.js"></script>
+    <script src="lib/underscore-1.8.3.min.js"></script>
+    <script src="lib/backbone-1.3.3-min.js"></script>
+</head>
+<body>
+<div id="container" class="col-mid-1">
+    <div id="main-nav"></div>
+    <p>Hello Paul</p>
+</div>
+</body>
+<footer>
+    <script src="js/home.js"></script>
+</footer>
+</html>
 
-include_once "app/index.php";
