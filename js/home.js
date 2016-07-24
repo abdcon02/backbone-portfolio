071c107c06b5114839ba310171b54727db7898a6
Connor Abdelnoor <connora@copiousinc.com>
2016-07-24 13:15:39 -0700

    finally render data in a view

--- js/home.js
+++ js/home.js
@@ -1,1 +1,66 @@
-$(document).ready(function(){var e=Backbone.Model.extend({url:"../data/nav.json",className:"nav-item"}),n=(Backbone.Collection.extend({model:e}),Backbone.View.extend({el:"#main-nav",initialize:function(){this.render()},render:function(){return this.$el.html("yo"),this}}));new n});
+$(document).ready(function() {
+
+    // Namespace the objects
+    var app = {};
+
+    app.NavItem = Backbone.Model.extend({
+        defaults: {
+            name: "Golf",
+            url: null
+        }
+    });
+
+    app.NavListCollection = Backbone.Collection.extend({
+        model: app.NavItem,
+        url: '../data/nav.json'
+    });
+
+
+    app.NavListView = Backbone.View.extend({
+        el: "#main-nav",
+
+
+        initialize: function() {
+            var scope = this;
+            this.collection.fetch({
+                success: function() {
+                    scope.render()
+                }
+            })
+        },
+
+        render: function() {
+            var scope = this;
+            this.collection.forEach(function(model) {
+                scope.output(model)
+            });
+            return this;
+        },
+
+        output: function(model) {
+            var html = "<div>" + model.get("name") + "</div>";
+            this.$el.append(html);
+        }
+    });
+
+    var testView = new app.NavListView({collection: new app.NavListCollection()});
+
+
+    // var hello = Backbone.View.extend({
+    //
+    //     el: '#container',
+    //
+    //     template: _.template("<h1> Hello <%= anything %> </h1>"),
+    //
+    //     initialize: function(){
+    //         this.render();
+    //     },
+    //
+    //     render: function(){
+    //         this.$el.html(this.template({anything: 'tester'}));
+    //     }
+    // });
+    //
+    // var helloView = new hello();
+
+});
--- source/js/home.js
+++ source/js/home.js
@@ -1,28 +1,66 @@
 $(document).ready(function() {
 
-    var NavItem = Backbone.Model.extend({
-        url: '../data/nav.json',
-        className: 'nav-item'
-    });
+    // Namespace the objects
+    var app = {};
 
-    var NavItemCollection = Backbone.Collection.extend({
-        model: NavItem
-    });
-
-    var NavListView = Backbone.View.extend({
-        el: '#main-nav',
-
-        initialize: function() {
-            this.render();
-        },
-
-        render: function() {
-            this.$el.html('yo');
-            return this;
+    app.NavItem = Backbone.Model.extend({
+        defaults: {
+            name: "Golf",
+            url: null
         }
     });
 
-    // var navCollection = new NavItemCollection;
-    var navContainerView = new NavListView();
+    app.NavListCollection = Backbone.Collection.extend({
+        model: app.NavItem,
+        url: '../data/nav.json'
+    });
+
+
+    app.NavListView = Backbone.View.extend({
+        el: "#main-nav",
+
+
+        initialize: function() {
+            var scope = this;
+            this.collection.fetch({
+                success: function() {
+                    scope.render()
+                }
+            })
+        },
+
+        render: function() {
+            var scope = this;
+            this.collection.forEach(function(model) {
+                scope.output(model)
+            });
+            return this;
+        },
+
+        output: function(model) {
+            var html = "<div>" + model.get("name") + "</div>";
+            this.$el.append(html);
+        }
+    });
+
+    var testView = new app.NavListView({collection: new app.NavListCollection()});
+
+
+    // var hello = Backbone.View.extend({
+    //
+    //     el: '#container',
+    //
+    //     template: _.template("<h1> Hello <%= anything %> </h1>"),
+    //
+    //     initialize: function(){
+    //         this.render();
+    //     },
+    //
+    //     render: function(){
+    //         this.$el.html(this.template({anything: 'tester'}));
+    //     }
+    // });
+    //
+    // var helloView = new hello();
 
 });
